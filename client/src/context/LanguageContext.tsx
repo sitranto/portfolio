b3f137b76cb65152ce15e548285612316af659e5
sitranto <anatoliyl2006@gmail.com>
2025-10-02 13:38:47 +0300

    Added LanguageContext

---
+++ client/src/context/LanguageContext.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import {Language} from "@/types/main";
+import {createContext, ReactNode, useContext, useState} from "react";
+
+interface LanguageContextProps {
+    language: Language,
+    setLanguage: (language: Language) => void,
+    t: (key: string) => string
+}
+
+const LanguageContext = createContext<LanguageContextProps | undefined>(undefined);
+
+const translations = {
+    en: {
+
+    },
+    ru: {
+
+    }
+};
+
+export function LanguageProvider({ children }: { children: ReactNode }) {
+    const [language, setLanguage] = useState<Language>('ru');
+
+    const t = (key: string) => {
+        return translations[language][key as keyof typeof translations['ru']] || key;
+    }
+
+    return (
+        <LanguageContext.Provider value={{ language, setLanguage, t }}>
+            {children}
+        </LanguageContext.Provider>
+    );
+}
+
+export function useLanguage() {
+    const context = useContext(LanguageContext);
+    if (!context) {
+        throw new Error("useLanguage() must be used within LanguageProvider");
+    }
+    return context;
+}
